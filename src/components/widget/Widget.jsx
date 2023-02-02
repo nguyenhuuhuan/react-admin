85bf77bf86091321d63fa2169aabe201357a9dbf
huan.nguyen <huan.nguyen@onemount.com>
2023-02-02 09:48:21 +0700

    second commit

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './widget.scss'
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import { BreakfastDiningOutlined } from '@mui/icons-material';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
 const Widget = ({type}) => {
 
     let data;
+
+    const amount = 100;
+    const diff = 20;
 
     switch(type){
         case "user":
@@ -14,58 +19,76 @@ const Widget = ({type}) => {
                 isMoney:false,
                 link:"See all users",
                 icon:(
-                    <PersonOutlineOutlinedIcon className='icon'/>
+                    <PersonOutlineOutlinedIcon className='icon' style={{
+                        color:"crimson",
+                        backgroundColor:"rgba(255,0,0,0.2)",
+                    }}
+                />
                 )
             };
-        break;
+            break;
         case "order":
-        data={
-            title:"USERS",
-            isMoney:false,
-            link:"See all users",
-            icon:(
-                <PersonOutlineOutlinedIcon className='icon'/>
-            )
-        }
-        break;
-        case "user":
-        data={
-            title:"USERS",
-            isMoney:false,
-            link:"See all users",
-            icon:(
-                <PersonOutlineOutlinedIcon className='icon'/>
-            )
-        }
-        break;
-        case "user":
-        data={
-            title:"USERS",
-            isMoney:false,
-            link:"See all users",
-            icon:(
-                <PersonOutlineOutlinedIcon className='icon'/>
-            )
-        }
-        break;
+            data={
+                title:"ORDERS",
+                isMoney:false,
+                link:"View all orders",
+                icon:(
+                    <ShoppingCartOutlinedIcon className='icon' style={{
+                        color:"goldenrod",
+                        backgroundColor:"rgba(218,165,32,0.2)",
+                        }}
+                    />
+                )
+            }
+            break;
+        case "earning":
+            data={
+                title:"EARNING",
+                isMoney:true,
+                link:"View net earning",
+                icon:(
+                    <MonetizationOnOutlinedIcon className='icon'
+                        style={{
+                            color:"green",
+                            backgroundColor:"rgba(0,128,0,0.2)",
+                        }}
+                    />
+                )
+            }
+            break;
+        case "balance":
+            data={
+                title:"BALANCE",
+                isMoney:true,
+                link:"See details",
+                icon:(
+                    <AccountBalanceWalletOutlinedIcon className='icon'
+                        style={{
+                            color:"purple",
+                            backgroundColor:"rgba(128,0,128,0.2)",
+                        }}
+                    />
+                )
+            }
+            break;
         default:
             break;
     }
   return (
     <div className='widget'>
         <div className="left">
-            <span className="title">USERS</span>
-            <span className="couter">123132</span>
-            <span className="link">See all users</span>
+            <span className="title">{data.title}</span>
+            <span className="couter">{data.isMoney && "$"} {amount}</span>
+            <span className="link">{data.link}</span>
 
 
         </div>
         <div className="right">
             <div className="percentage negative">
                 <KeyboardArrowUpOutlinedIcon/>
-                20%
+                {diff} %
             </div>
-            <PersonOutlineOutlinedIcon className='icon'/>
+            {data.icon}
         </div>
     </div>
   )
